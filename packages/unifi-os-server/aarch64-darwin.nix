e41900dc52ac9dfa5588bb0ec643c5958c3eb545
Robert Cambridge <robert@cambridge.me>
2026-05-06 21:44:24 +0200

    simulate outdated versions

--- packages/unifi-os-server/aarch64-darwin.nix
+++ packages/unifi-os-server/aarch64-darwin.nix
@@ -1,5 +1,5 @@
 {
-  installerVersion = "5.0.6";
-  url = "https://fw-download.ubnt.com/data/unifi-os-server/b114-macOS-dmg-arm64-5.0.6-52fc45da-b544-4a72-a162-ad5c28b93989.dmg";
-  sha256 = "sha256-7MdmF1KF5EbaWl93T2W8wfl2V/umFVwpMAIvoDgLM90=";
+  installerVersion = "";
+  url = "";
+  sha256 = "";
 }
--- packages/unifi-os-server/x86_64-darwin.nix
+++ packages/unifi-os-server/x86_64-darwin.nix
@@ -1,5 +1,5 @@
 {
-  installerVersion = "5.0.6";
-  url = "https://fw-download.ubnt.com/data/unifi-os-server/b290-macOS-dmg-amd64-5.0.6-a165bb5f-f865-476a-b4bd-703e0d3d4ca1.dmg";
-  sha256 = "sha256-jgMJd8a/TpweLmXuOv1xAztnJkFnz1Ct0/rUirlUTUA=";
+  installerVersion = "";
+  url = "";
+  sha256 = "";
 }
